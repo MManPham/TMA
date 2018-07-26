86fd3f8b180551256190dab2c6442376d0a7fca8
MManPham <38095980+MManPham@users.noreply.github.com>
2018-07-26 23:04:24 +0700

    angular form

---
+++ RoutingTemplate/routingTemplate/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule,routingComponent } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavComponent } from './nav/nav.component';
+;
+
+@NgModule({
+  declarations: [ 
+    AppComponent,
+    routingComponent,
+    PageNotFoundComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
